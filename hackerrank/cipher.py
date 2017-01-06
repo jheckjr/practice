66ef7167774ece08e156e14c57e4660bb542bc3b
Joel <joel.heckjr@gmail.com>
2017-01-06 23:18:36 +0000

    Complete Cipher

--- hackerrank/cipher.py
+++ hackerrank/cipher.py
@@ -3,24 +3,29 @@ Problem:
 Hacker Rank - Cipher
 '''
 
-### TODO ###
-N, K = [int(x) for x in input().split(' ')]
-S = [int(x) for x in list(input())]
-
-# number of ones in last K-1 bits
-numOnes = S[0]
-B = [0] * N
-B[0] = S[0]
-
-for i in range(1,N):
-    B[i] = (numOnes % 2) ^ S[i]
-    numOnes += B[i]
+def solve(f):
+    N, K = [int(x) for x in f.readline().split(' ')]
+    S = [int(x) for x in list(f.readline())]
     
-    if K-1 <= i:
-        numOnes -= B[i-(K-1)]
+    # number of ones in last K-1 bits
+    numOnes = S[0]
+    B = [0] * N
+    B[0] = S[0]
     
-print(''.join([str(x) for x in B]))
+    for i in range(1,N):
+        B[i] = (numOnes % 2) ^ S[i]
+        numOnes += B[i]
+        
+        if K-1 <= i:
+            numOnes -= B[i-(K-1)]
+        
+    return ''.join([str(x) for x in B])
 
-'''
-1001010
-'''
+def test():
+    with open('cipher.txt', 'r') as f:
+        result = solve(f)
+        assert result == '1001010'
+        
+    print("Test passed")
+
+test()
